9e206e5bb0ae9751f789970c72a38ab1d7eceefe
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-02-10 08:47:04 -0700

    Added code for query 1 on assignment 2

--- CarsonASQL2.sql
+++ CarsonASQL2.sql
@@ -1,59 +1,65 @@
 -- Query 1
 SELECT
-   firstName AS first_name
-FROM
-   students;
-
--- Query 2
-SELECT
-   phone
-FROM
-   students;
-
--- Query 3
-SELECT
-   firstName AS first_name
-FROM 
-   students;
-
--- Query 4
-SELECT
-   studentID AS student_id
-FROM
-   students;
-
--- Query 5
-SELECT
+   firstName || ' ' || lastName AS full_name,
+   phone AS phone_number,
    ZIP AS zip
 FROM
-   students;
+   students
+WHERE
+   city = 'Medford'
+ORDER BY
+   full_name ASC;
 
--- Query 6
-SELECT
-   studentID AS student_id
-FROM
-   students;
+-- -- Query 2
+-- SELECT
+--    phone
+-- FROM
+--    students;
 
--- Query 7
-SELECT
-   firstName AS first_name
-FROM
-   professors;
+-- -- Query 3
+-- SELECT
+--    firstName AS first_name
+-- FROM 
+--    students;
 
--- Query 8
-SELECT
-   firstName AS first_name
-FROM
-   students;
+-- -- Query 4
+-- SELECT
+--    studentID AS student_id
+-- FROM
+--    students;
 
--- Query 9
-SELECT 
-   studentID AS student_id
-FROM
-   students;
+-- -- Query 5
+-- SELECT
+--    ZIP AS zip
+-- FROM
+--    students;
 
--- Query 10
-SELECT
-   first_name
-FROM
-   dual;
+-- -- Query 6
+-- SELECT
+--    studentID AS student_id
+-- FROM
+--    students;
+
+-- -- Query 7
+-- SELECT
+--    firstName AS first_name
+-- FROM
+--    professors;
+
+-- -- Query 8
+-- SELECT
+--    firstName AS first_name
+-- FROM
+--    students;
+
+-- -- Query 9
+-- SELECT 
+--    studentID AS student_id
+-- FROM
+--    students;
+
+-- -- Query 10
+-- SELECT
+--    first_name
+-- FROM
+--    dual;
