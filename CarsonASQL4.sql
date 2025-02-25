438d07a47ab1a25bae3b144f7b1113b50278536a
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-02-25 08:11:07 -0700

    Added stubs for assignment 4

---
+++ CarsonASQL4.sql
@@ -0,0 +1,69 @@
+-- Query 1
+SELECT
+   tuitionBalance
+FROM
+   students
+;
+
+-- Query 2
+SELECT
+   admissionDate
+FROM
+   students
+;
+
+-- Query 3
+SELECT
+   score
+FROM
+   assignmentScore
+;
+
+-- Query 4
+SELECT
+   city
+FROM
+   students
+;
+
+-- Query 5
+SELECT
+   sectionID AS section_id
+FROM
+   registration
+;
+
+-- Query 6
+SELECT
+   sectionID AS section_id
+FROM
+   registration
+;
+
+-- Query 7
+SELECT
+   sectionID AS section_id
+FROM
+   registration
+;
+
+-- Query 8
+SELECT
+   score
+FROM
+   assignmentScore
+;
+
+-- Query 9
+SELECT
+   city
+FROM
+   students
+;
+
+-- Query 10
+SELECT
+   sectionID AS section_id
+FROM
+   assignmentScore
+;
