44622765076cd1d2225e18238925d420623a6603
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-03-20 08:08:34 -0600

    Created sql 6 assignment and added stubs

---
+++ CarsonASQL6.sql
@@ -0,0 +1,69 @@
+-- Query 1
+SELECT
+   firstName || ' ' || lastName AS full_name
+FROM
+   students
+;
+
+-- Query 2
+SELECT
+   firstName || ' ' || lastName AS full_name
+FROM
+   students
+;
+
+-- Query 3
+SELECT
+   lastName AS last_name
+FROM
+   professors
+;
+
+-- Query 4
+SELECT
+   studentID AS student_id
+FROM
+   registration
+;
+
+-- Query 5
+SELECT
+   subjectCode
+FROM
+   courses
+;
+
+-- Query 6
+SELECT
+   city
+FROM
+   professors
+;
+
+-- Query 7
+SELECT
+   assignmentTypeID
+FROM
+   assignmentScore
+;
+
+-- Query 8
+SELECT
+   firstName AS first_name
+FROM
+   professors
+;
+
+-- Query 9
+SELECT
+   firstName AS first_name
+FROM
+   students
+;
+
+-- Query 10
+SELECT
+   admissionDate
+FROM
+   students
+;
