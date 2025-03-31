67cfb597ba098686e36e04976f77fbce61b5c060
CAngell0 <carsonjesseangell.learn@gmail.com>
2025-03-31 08:01:43 -0600

    Added stubs and base for assignment 7

---
+++ CarsonASQL7.sql
@@ -0,0 +1,67 @@
+-- Query 1
+SELECT
+   firstName || ' ' || lastName AS full_name
+FROM
+   professors
+;
+
+-- Query 2
+SELECT
+   studentID AS student_id
+FROM
+   assignmentScore
+;
+
+
+-- Query 3
+SELECT
+   subjectCode
+FROM
+   courses
+;
+
+
+-- Query 4
+SELECT
+   courseID
+FROM
+   courses
+;
+
+-- Query 5
+SELECT
+   firstName || ' ' || lastName AS full_name
+FROM
+   students
+;
+
+-- Query 6
+SELECT
+   firstName || ' ' || lastName AS full_name
+FROM
+   students
+;
+
+
+-- Query 7
+SELECT
+   subjectCode
+FROM
+   courses
+;
+
+
+-- Query 8
+SELECT
+   firstName AS first_name
+FROM
+   students
+;
+
+
+-- Query 9
+-- Get the classes in which the class is full. List the instructor, subject code + title and start date.
+
+-- Query 10
+
+
